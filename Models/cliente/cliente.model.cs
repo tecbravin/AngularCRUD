cd497928625b582abb8ff5a47a8db340736400f3
Marwin Bravin <marwin.bravin@qintess.com>
2022-05-05 17:34:11 -0300

    start

---
+++ Models/cliente/cliente.model.cs
@@ -0,0 +1,8 @@
+public class ClienteModel {
+  public string Nome { get; set; }
+  public string CPF { get; set; }
+  public Date Data Nascimento { get; set; }
+  public string Renda Mensal { get; set; }
+  public string E-mail { get; set; }
+  public Date Data_Cadastro { get; set; }
+ }
